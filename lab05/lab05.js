8a5bf132be1960b74c9a48d204b8fc972a500427
Amumeris <vesper.port@gmail.com>
2021-05-04 15:40:13 +0800

    UPDATE LAB05 HOMEWORK

---
+++ lab05/lab05.js
@@ -0,0 +1,20 @@
+d3.text("data.csv", function (data) {
+    var parsedCSV = d3.csv.parseRows(data);
+
+    d3.select("body")
+        .selectAll("div")
+        .data(parsedCSV)
+        .enter()
+        .append("div")
+        .filter(function (d, i) {
+            return (i != 0);
+        })
+        .attr("class", "bar")
+        .style("height", function (d) {
+            return 7 * d[1] + "px";
+        })
+        .append("text")
+        .text(function (d) {
+            return d[1];
+        })
+});
